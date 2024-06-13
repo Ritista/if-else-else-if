ddb4d986928cdd7d77cb39e01e0bd584f417c8cc
Ritista Shrestha <ritista99@gmail.com>
2024-06-13 14:58:41 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+//odd number or even number
+//if, else
+let d=Number(prompt("enter number"));
+console.log(typeof a)
+ 
+if(d%2==0){
+    console.log("it is an even number");
+}
+else{
+    console.log("it is an odd number");
+}
+
+//if, else, else if
+let c=true;
+console.log(typeof a)
+if(c==false){
+    console.log("it is false");
+}
+else if(c==true){
+    console.log("It is true")
+}
+else{
+    console.log("its none");
+}
+
+
+let b="friday";
+console.log(typeof a)
+if(b=="saturday"){
+    console.log("it is saturday");
+}
+else if(b=="friday"){
+    console.log("it is friday");
+}
+else{
+    console.log("its none");
+}
+
+let a=Number(prompt("enter your age"));
+console.log(typeof a)
+
+if(a>=18 && a<=59){
+    console.log("you can vote");
+}
+else{
+    console.log("you cannot vote");
+}
+
